c832d55e4a22cc6e50c1e4e37c72a2a3a30e14e4
Robert Vojta <robert@balena.io>
2018-11-21 14:49:55 +0100

    Publish on tag only
    Change-type: patch
    Signed-off-by: Robert Vojta <robert@balena.io>

--- ci/publish.sh
+++ ci/publish.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    # repo.yml.type == rust-* (rust-crate, rust-crate-wasm)
-    cargo login "$CARGO_API_TOKEN"
-    cargo publish
+if [ -z ${TRAVIS_TAG+x} ]; then
+    # Do not publish if there's no TRAVIS_TAG set
+    echo "TRAVIS_TAG not set, skipping"
+    exit 0
 fi
+
+# Giovanni, following must be called only when:
+#
+#  * `branch == master`
+#  * on a commit with bumped version
+
+# repo.yml.type == rust-* (rust-crate, rust-crate-wasm)
+cargo login "$CARGO_API_TOKEN"
+cargo publish
